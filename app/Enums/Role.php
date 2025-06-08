c7f899d493c7dcaa4ec6f0679a295aad74cef3e3
Kid Max <kidmax285@gmail.com>
2025-06-08 14:39:06 +0600

    first commit

---
+++ app/Enums/Role.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Modules\User\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * Class RoleType
+ * @package App\Enums
+ */
+final class Role extends Enum
+{
+    public const SUPER_ADMIN = 'super_admin';
+    public const STORE_OWNER = 'store_owner';
+    public const STAFF = 'staff';
+    public const CUSTOMER = 'customer';
+}
